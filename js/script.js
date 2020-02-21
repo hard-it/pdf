99c3b09212897cfa5ac3d282accdb32d10a904a3
Nikolay Lamanov <59505751+hard-it@users.noreply.github.com>
2020-02-21 15:46:22 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$('.pdf').on('click', function(){
+var docInfo = {
+		
+		info: {
+			title:'Тестовый документ PDF',
+			author:'Viktor',
+			subject:'Theme',
+			keywords:'Ключевые слова'
+		},
+		
+		pageSize:'A4',
+		pageOrientation:'landscape',
+		pageMargins:[50,50,50,50],
+		
+		header:function(currentPage,pageCount) {
+			return {
+				text: currentPage.toString() + 'из' + pageCount,
+				alignment:'right',
+				margin:[0,30,10,50]
+			}
+		},
+		
+		footer:[
+			{
+				text:'Агронаем',
+				alignment:'center',
+			}
+		],
+		
+		content: [
+		
+			{
+				table:{
+					widths:['50%','50%'],
+					
+					body:[
+						['Первая','Вторая'],
+						['Первая','Вторая'],
+						[{text:'текстовое содержимое', bold:true}, 'Вторая']
+					],
+					headerRows:1
+				}
+			},
+			
+			
+		],
+		
+		styles: {
+			header: {
+				fontSize:25,
+				bold:true,
+				alignment:'right'
+			}
+		}
+	}
+	
+	pdfMake.createPdf(docInfo).download('name.pdf');
+	
+});
+	
+
+
